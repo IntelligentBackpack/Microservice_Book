4c691012dd8f52d9fe793197e1e3591e6464a4fc
Matteo Lazzari <matteo.lazzari7@studio.unibo.it>
2023-05-05 11:36:16 +0200

    First push

---
+++ sources/main/routes/Create.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+const router = Router();
+export default router;
+
+
+//this route manage the login a user
+router.post('', async (req: {body: string}, res) => {
+
+});
+
